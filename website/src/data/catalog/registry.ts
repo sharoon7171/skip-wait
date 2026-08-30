2141fd9359bf5add34d0a3921283330e4a7ba1e6
Sharoon <sharoon7171@gmail.com>
2026-08-31 02:24:19 +0500

    register BBLINK and BBMKTS Sub to Unlock in catalog

--- website/src/data/catalog/registry.ts
+++ website/src/data/catalog/registry.ts
@@ -11,6 +11,8 @@ import { bypass as apktealBypass } from './bypasses/apkteal-bypass';
 import { bypass as apkvisionBypass } from './bypasses/apkvision-bypass';
 import { bypass as apunkagamesBypass } from './bypasses/apunkagames-bypass';
 import { bypass as arolinksBypass } from './bypasses/arolinks-bypass';
+import { bypass as bblinkBypass } from './bypasses/bblink-bypass';
+import { bypass as bbmktsSubtounlockBypass } from './bypasses/bbmkts-subtounlock-bypass';
 import { bypass as boostellarBstshrtBypass } from './bypasses/boostellar-bstshrt-bypass';
 import { bypass as boostylinkBypass } from './bypasses/boostylink-bypass';
 import { bypass as clipiBypass } from './bypasses/clipi-bypass';
@@ -142,6 +144,8 @@ export const bypasses: readonly SupportedBypass[] = [
   apkvisionBypass,
   apunkagamesBypass,
   arolinksBypass,
+  bblinkBypass,
+  bbmktsSubtounlockBypass,
   boostellarBstshrtBypass,
   boostylinkBypass,
   clipiBypass,
